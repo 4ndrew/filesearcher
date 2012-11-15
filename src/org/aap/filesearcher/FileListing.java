63217481f98697b47826522a93d63baff1c09c76
Andrew Porokhin <andrew.porokhin@gmail.com>
2012-11-15 22:04:43 +0400

    Memory optimization for big directories

--- src/org/aap/filesearcher/FileListing.java
+++ src/org/aap/filesearcher/FileListing.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Andrew Porokhin. All rights reserved.
+ * Copyright 2011-2012 Andrew Porokhin. All rights reserved.
  * Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
  */
 
@@ -9,13 +9,15 @@ import org.aap.filesearcher.executor.TaskAcceptor;
 import org.apache.log4j.Logger;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.LinkedList;
 
 /**
- * File listing.
+ * Recursive file listing optimized for fast performance and less memory usage (maximum on Java6, it could be
+ * optimized for Java7).
  */
 public class FileListing implements Runnable {
-    private final Logger logger = Logger.getLogger(FileListing.class);
+    private final static Logger logger = Logger.getLogger(FileListing.class);
     private final File rootDirectory;
     private final TaskAcceptor<FileSearchBean> taskQueue;
     private final LinkedList<File> directoryQueue;
@@ -35,18 +37,27 @@ public class FileListing implements Runnable {
         try {
             directoryQueue.addLast(rootDirectory);
 
-            while (directoryQueue.size() > 0) {
-                File rootNode = directoryQueue.removeFirst();
-                for (File f : rootNode.listFiles()) {
-                    if (f.isDirectory()) {
-                        directoryQueue.add(f);
-                    } else if (f.isFile()) {
-                        pushSearchTask(f);
+            final FileFilter fileTraversalFilter = new FileFilter() {
+                @Override
+                public boolean accept(File pathName) {
+                    if (pathName.isDirectory()) {
+                        directoryQueue.add(pathName);
+                    } else if (pathName.isFile()) {
+                        try {
+                            taskQueue.push(new FileSearchBean(pathName));
+                        } catch (InterruptedException e) {
+                            logger.warn("File listing interrupted", e);
+                            Thread.currentThread().interrupt();
+                        }
                     }
+                    return false;
                 }
+            };
+            while (directoryQueue.size() > 0) {
+                final File rootNode = directoryQueue.removeFirst();
+                //noinspection ResultOfMethodCallIgnored
+                rootNode.listFiles(fileTraversalFilter);
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace(System.err);
         } finally {
             logger.info("Listing finished.");
             taskQueue.signalEndOfData();
